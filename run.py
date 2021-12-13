da2f2d08c0656fa745040882a59c8c67fc1956ce
Alopmel <albalopezmelian@gmail.com>
2021-12-13 12:54:19 +0000

    Added the dog's name and calculated the daily and monthly amount of food

--- run.py
+++ run.py
@@ -1,3 +1,17 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+print("Hello, we are a store dedicated to the health and happiness of your pet.")
+print("let's start by filling in your dog's file with a few simple questions\n")
+
+
+def calculate_amount():
+    """
+    Add the pet's name and calculated its daily and monthly amount of food
+    """
+    pet_name_str = input("What is your dog's name? ")
+    weight = int(input("Insert your dog's weight: ")) 
+    daily_amount = (weight * 0.025) * 1000
+    daily_round = round(daily_amount,2)
+    monthly_amount = (daily_amount * 30) / 1000
+    monthly_round = round(monthly_amount,2)
+    print(f"the daily amount that {pet_name_str} has to eat is {daily_round} gr. Therefore we recommend you buy {monthly_round} kg per month")
+
+calculate_amount()  
